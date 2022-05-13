ae1ecea20cd6e4b5e0e8655176f4dd06950d4edd
lam268 <lamlv@tokyotechlab.com>
2022-05-13 10:29:48 +0700

    init base

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,49 @@
-/* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  plugins: ['unused-imports'],
+
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'unused-imports/no-unused-imports-ts': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended',
+    'eslint:recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+};
